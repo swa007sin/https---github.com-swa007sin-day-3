62aa8ddeb0c9d3a42e20785102ea9538a8b194cd
Swaraj Singh Chouhan <swarajsingh888.ss@gmail.com>
2021-12-05 14:40:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var obj = [
+    { person: "Name 1", age: "2", company: "GUVI" },
+    { person: "Name 2", age: "5", company: "GUVI geek" },
+    { person: "Name 3", age: "8", company: "GUVI geek network" },
+  ];
+  for(let i=0; i<obj.length;i++)
+  {
+      console.log(object[i])
+  }
